80485870568cd5cbc20d1a4612ff0fb3e0613481
Tristan La Rusic <tristan@larusic.com>
2023-03-27 10:40:54 -0300

    prompt log

--- utils/index.ts
+++ utils/index.ts
@@ -6,7 +6,7 @@ export enum OpenAIModel {
 }
 
 export const OpenAIStream = async (prompt: string) => {
-  console.log("Starting OpenAI stream...");
+  console.log("Prompting OpenAI: ", prompt);
 
   const apiKey = process.env.OPEN_AI_API_KEY;
 
