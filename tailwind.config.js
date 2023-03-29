db0946a09821d189a7a8bdc8afcd078979b9330f
Tristan La Rusic <tristan@larusic.com>
2023-03-28 21:49:35 -0300

    add fcnb colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'fcnb-blue': '#04519c',
+        'fcnb-yellow': '#fbe471',
+      },
+    },
   },
   plugins: [],
 }
