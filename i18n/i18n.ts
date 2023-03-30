5ab2694c4c642a397f74c98b93a3b4b40f0dab77
Tristan La Rusic <tristan@larusic.com>
2023-03-30 09:28:05 -0300

    Update footer disclaimer

--- i18n/i18n.ts
+++ i18n/i18n.ts
@@ -23,7 +23,7 @@ i18n
           slogan: 'Search the FCNB knowledgebase using AI!',
           hint: 'What is FCNB?',
           poweredBy: 'Powered by GPT-3.5',
-          disclaimer: "*This website is a proof of concept and intended for testing purposes only, any information presented here is sourced from <1 className='font-normal hover:underline' href='https://www.fcnb.ca/en' target='_blank'>fcnb.ca</1> and should not be considered final or official."
+          disclaimer: "*This website is a proof of concept and intended for testing purposes only. All information presented here was sourced from <1 className='font-normal hover:underline' href='https://www.fcnb.ca/en' target='_blank'>fcnb.ca</1> and is accurate as of March 18th, 2023. Please note that this information should not be considered final or official."
         }
       },
       fr: {
@@ -32,7 +32,7 @@ i18n
           slogan: 'Recherchez la base de connaissances de FCNB en utilisant l\'IA !',
           hint: 'Qu\'est-ce que la FCNB ?',
           poweredBy: 'Propulsé par GPT-3.5',
-          disclaimer: "*Ce site Web est une preuve de concept et est destiné uniquement à des fins de test. Toute information présentée ici est sourcée de <1 className='font-normal hover:underline' href='https://www.fcnb.ca/fr' target='_blank'>fcnb.ca</1> et ne doit pas être considérée comme finale ou officielle."
+          disclaimer: "*Ce site Web est une preuve de concept et destiné à des fins de test uniquement. Toutes les informations présentées ici ont été sourcées de <1 className='font-normal hover:underline' href='https://www.fcnb.ca/fr' target='_blank'>fcnb.ca</1> et sont exactes jusqu'au 18 mars 2023. Veuillez noter que ces informations ne doivent pas être considérées comme finales ou officielles."
         }
       }
     }
