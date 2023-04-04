5521bbe7a704afa9e2ac6266d9abd50bafce7ab5
Tristan La Rusic <tristan@larusic.com>
2023-04-03 21:38:07 -0300

    add icons to documents

--- components/Answer/Answer.tsx
+++ components/Answer/Answer.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Image from 'next/image'
 import { Document } from "langchain/document";
 import styles from "./answer.module.css";
 
@@ -26,11 +27,20 @@ export const Answer: React.FC<AnswerProps> = ({ text, showDocuments, documents }
           {word}{" "}
         </span>
       ))}
-      <div className='mt-6 divide-y divide-slate-200'>
-        {showDocuments && documents.map((doc, index) => (
-          <div key={index} className={`mt-2 hover:underline ${styles.fadeIn}`} style={{ animationDelay: `${index * 0.001}s` }}>
+      <div className={`font-bold text-xl pt-6 ${styles.fadeIn}`}>
+        Read More
+      </div>
+      <div className='mt-6 pb-6 divide-y divide-slate-200'>
+        {showDocuments && documents.map((doc, index) => {
+          const type = doc.metadata.source.split("/")[0];
+          const image = type === "pdf" ? "/favicon_pdf.png" : "/favicon_black.ico";
+          return (<div
+            key={index}
+            className={`mt-2 hover:underline flex flex-row space-x-2 ${styles.fadeIn}`}
+            style={{ animationDelay: `${index * 0.3}s` }}>
+              <Image src={image} width={22} height={8} alt={type} />
             <a href={doc.metadata.url} target='_blank'>{doc.metadata.title}</a>
-          </div>),
+          </div>)},
         )}
       </div>
     </div>
