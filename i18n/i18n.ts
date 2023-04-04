778e0639e159f913764f442e0399a3a880fdf0d6
Tristan La Rusic <tristan@larusic.com>
2023-04-04 07:23:00 -0300

    French Read more

--- i18n/i18n.ts
+++ i18n/i18n.ts
@@ -25,7 +25,8 @@ i18n
           poweredBy: 'Powered by GPT-3.5',
           disclaimer: "*This website is a proof of concept and intended for testing purposes only. All information presented here was sourced from <1 className='font-normal hover:underline' href='https://www.fcnb.ca/en' target='_blank'>fcnb.ca</1> and is accurate as of March 18th, 2023.",
           error: "An error occurred while searching.",
-          query: "Please enter a query"
+          query: "Please enter a query",
+          readMore: "Read More"
         }
       },
       fr: {
@@ -36,7 +37,8 @@ i18n
           poweredBy: 'Propulsé par GPT-3.5',
           disclaimer: "*Ce site Web est une preuve de concept et destiné à des fins de test uniquement. Toutes les informations présentées ici ont été sourcées de <1 className='font-normal hover:underline' href='https://www.fcnb.ca/fr' target='_blank'>fcnb.ca</1> et sont exactes jusqu'au 18 mars 2023.",
           error: "Une erreur s'est produite lors de la recherche.",
-          query: "Veuillez entrer une requête."
+          query: "Veuillez entrer une requête.",
+          readMore: "En savoir plus"
         }
       }
     }
