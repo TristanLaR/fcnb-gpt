fdf2a1ab33ab8d58439cbc47e1a0d2792c8259e1
Tristan La Rusic <tristan@larusic.com>
2024-12-05 12:48:31 -0400

    white favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,15 @@
 import { Inter } from 'next/font/google'
 import { Providers } from './providers'
 import '../styles/globals.css'
+import { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  icons: {
+    icon: '/favicon_white.ico',
+  },
+}
 
 export default function RootLayout({
   children,
